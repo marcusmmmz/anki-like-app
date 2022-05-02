eabb877f400bab2dca5dc03b8c18769fa67a3a32
Marcus <marcusizabel230@gmail.com>
2022-05-02 01:24:36 -0300

    import export db

--- src/routes/settings.svelte
+++ src/routes/settings.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+	import { db } from "$lib/db";
 	import { settings } from "$lib/stores";
+	import { exportDB } from "dexie-export-import";
+	import download from "downloadjs";
 
 	let reviewTime = $settings.reviewTimeFormula;
 
@@ -13,9 +16,35 @@
 			console.log(error);
 		}
 	}
+
+	async function importData(file: File) {
+		await db.import(file);
+	}
+	async function exportData() {
+		let exported = await exportDB(db);
+		download(exported, "ala_data.json", "text/plain");
+	}
+
+	function onImportChange(
+		e: Event & {
+			currentTarget: EventTarget & HTMLInputElement;
+		}
+	) {
+		let file = e.currentTarget.files?.item(0);
+		if (file) {
+			importData(file);
+		}
+	}
 </script>
 
 <button on:click={settings.clear}> Reset settings </button>
+<br />
+<label>
+	import
+	<input on:change={onImportChange} type="file" />
+</label>
+<br />
+<button on:click={exportData}> export data </button>
 <h2>advanced</h2>
 <label>
 	Review time: <input type="text" bind:value={reviewTime} />
