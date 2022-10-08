a1684f889b170a385d3b0d2926e07086c4ef69bd
marcusmmmz <marcusizabel230@gmail.com>
2022-10-08 18:45:37 -0300

    back in manage page as textarea

--- src/routes/cards/+page.svelte
+++ src/routes/cards/+page.svelte
@@ -13,28 +13,62 @@
 	}
 </script>
 
-<form
-	action=""
-	on:submit|preventDefault={() =>
-		addCard({ front, back, box: 1, lastReview: null, nextReview: Date.now() })}
->
-	<label for=""> front </label>
-	<input bind:value={front} type="text" /><br />
-	<label for=""> back </label>
-	<input bind:value={back} type="text" /><br />
-	<input type="submit" />
-</form>
+<div class="container">
+	<form
+		action=""
+		on:submit|preventDefault={() =>
+			addCard({
+				front,
+				back,
+				box: 1,
+				lastReview: null,
+				nextReview: Date.now(),
+			})}
+	>
+		<label>
+			front
+			<input bind:value={front} type="text" />
+		</label>
+		<label>
+			back
+			<textarea bind:value={back} rows="4" cols="40" type="text" />
+		</label>
+		<input type="submit" />
+	</form>
 
-<div class="card-container">
-	{#each $cards as card (card.id)}
-		<div class="card">
-			<p>{card.front}, {card.back}</p>
-			<button on:click={() => deleteCard(card.id)}>delete</button>
-		</div>
-	{/each}
+	<div class="card-container">
+		{#each $cards as card (card.id)}
+			<div class="card">
+				<p>{card.front}, {card.back}</p>
+				<button on:click={() => deleteCard(card.id)}>delete</button>
+			</div>
+		{/each}
+	</div>
 </div>
 
 <style>
+	.container {
+		margin-top: 16px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	form {
+		display: flex;
+		flex-direction: column;
+		gap: 16px;
+
+		width: 50%;
+	}
+
+	label {
+		display: flex;
+		justify-content: space-between;
+		width: 100%;
+		font-size: 24px;
+	}
+
 	.card-container {
 		display: flex;
 		flex-direction: column;
